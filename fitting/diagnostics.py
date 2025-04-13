Possible fixing the numerical issues

fitting/diagnostics.py
@@ -12,18 +12,24 @@ from . import regression
 from .plotting.plots import makeDiagnosticPlots, makeCovariancePlots, plotRaw
 from .predictive import makePosteriorPred, makePValuePlots
 from .utils import chi2Bins
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 def plotDiagnostics(save_dir, trained_model, **kwargs):
     model = regression.loadModel(trained_model)
-    _, coupling, mt, mx = trained_model.metadata["signal_name"].split("_")
+    tm = trained_model.metadata
+    coupling, mt, mx = tm["coupling"], tm["mt"], tm["mx"]
     mt, mx = float(mt), float(mx)
     all_data, train_mask = regression.getModelingData(trained_model)
+    extra_noise = None
+    if trained_model.learned_noise:
+        extra_noise = model.likelihood.second_noise
+    logger.info(f"Diagonstics: extra noise is {extra_noise}")
+
     pred_dist = regression.getPosteriorProcess(
-        model,
-        all_data,
-        trained_model.transform,
-        # extra_noise=model.likelihood.second_noise
+        model, all_data, trained_model.transform, extra_noise=extra_noise
     )
 
     pred_data = DataValues(all_data.X, pred_dist.mean, pred_dist.variance, all_data.E)
@@ -60,6 +66,7 @@ def plotDiagnostics(save_dir, trained_model, **kwargs):
             obj.savefig((save_dir / name).with_suffix(f".{ext}"))
             plt.close(obj)
 
+
     diagnostic_plots = makeDiagnosticPlots(
         pred_data,
         all_data,
@@ -71,10 +78,10 @@ def plotDiagnostics(save_dir, trained_model, **kwargs):
 
     saveFunc("chi2_info", data)
 
+    makePValuePlots(pred_dist, all_data, train_mask, saveFunc)
     makePosteriorPred(pred_dist, all_data, saveFunc, train_mask)
     for point in [[mt, mx / mt]]:
         makeCovariancePlots(model, trained_model.transform, all_data, point, saveFunc)
-    makePValuePlots(pred_dist, all_data, train_mask, saveFunc)
 
 
 def plotCovarsForPoints(save_dir, trained_model, points):
@@ -146,7 +153,7 @@ def main(args):
     mplhep.style.use("CMS")
 
     out = args.outdir or Path(args.input).parent
-    m = torch.load(args.input,  weights_only=False )
+    m = torch.load(args.input, weights_only=False)
     plotDiagnostics(out, m)
 
 
@@ -154,7 +161,7 @@ def runEigens(args):
     import torch
 
     out = args.outdir or Path(args.input).parent
-    m = torch.load(args.input,  weights_only=False )
+    m = torch.load(args.input, weights_only=False)
     plotEigenvars(out, m, args.min_frac)
 
 
@@ -162,7 +169,7 @@ def runCovars(args):
     import torch
 
     out = args.outdir or Path(args.input).parent
-    m = torch.load(args.input,  weights_only=False )
+    m = torch.load(args.input, weights_only=False)
     plotCovarsForPoints(out, m, args.points)
 
 

fitting/estimate.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import logging
 import pickle as pkl
 from pathlib import Path
@@ -80,7 +81,7 @@ def regress(
         window,
         iterations=iterations,
         use_cuda=use_cuda,
-        learn_noise=False,
+        learn_noise=True,
         lr=learning_rate,
         validate_function=validate,
     )
@@ -101,7 +102,9 @@ def estimateSingle2DWithWindow(
     rebin_signal=1,
     use_other_model=None,
     use_other_kernel=None,
+    extra_metadata=None,
 ):
+    extra_metadata = extra_metadata or {}
     signal_injections = signal_injections or [0.0, 1.0, 4.0, 16.0]
     sig_dir = base_dir  # / signal_name
     sig_dir.mkdir(exist_ok=True, parents=True)
@@ -122,6 +125,23 @@ def estimateSingle2DWithWindow(
 
     windowPlots2D(signal_regression_data, window, saveFunc)
     torch.save(sd, sig_dir / "signal_data.pth")
+    _, coupling, stop, chi = signal_name.split("_")
+    model_data = {
+        "coupling": coupling,
+        "mt": int(stop),
+        "mx": int(chi),
+        "x_bounds": [
+            float(signal_regression_data.X[:, 0].min()),
+            float(signal_regression_data.X[:, 0].max()),
+        ],
+        "y_bounds": [
+            float(signal_regression_data.X[:, 1].min()),
+            float(signal_regression_data.X[:, 1].max()),
+        ],
+    }
+
+    model_data.update(extra_metadata)
+
     for r in signal_injections:
         print(window)
         save_dir = sig_dir / f"inject_r_{str(round(r,3)).replace('.','p')}"
@@ -141,15 +161,17 @@ def estimateSingle2DWithWindow(
             trained_model = regress(
                 to_estimate,
                 base_dir,
-                min_counts=-1,
+                # min_counts=20,
                 window=window,
                 use_cuda=True,
                 iterations=iterations,
                 learning_rate=learning_rate,
             )
-        trained_model.metadata.update(
-            {"signal_injected": r, "signal_name": signal_name}
-        )
+        this_injection_data = {**model_data, "signal_injected": r}
+        trained_model.metadata.update(this_injection_data)
+        with open(save_dir / "metadata.json", "w") as f:
+            json.dump(this_injection_data, f, indent=2)
+
         # trained_model.metadata.update(add_metadata)
         torch.save(trained_model, save_dir / "bkg_estimation_result.pth")
         plotDiagnostics(save_dir, trained_model)
@@ -270,18 +292,38 @@ def estimateSingle2D(
             signal_hist = signal_hist[
                 hist.rebin(rebin_signal), hist.rebin(rebin_signal)
             ]
-            estimateSingle2DWithWindow(
-                signal_name,
-                signal_hist,
-                bkg_hist,
-                window,
-                Path(base_dir) / f"inject_{name}",
-                rebin_signal=rebin_signal,
-                **kwargs,
-            )
+            with (
+                gpytorch.settings.fast_computations(
+                    covar_root_decomposition=False, log_prob=False, solves=False
+                ),
+                gpytorch.settings.fast_pred_samples(state=False),
+                gpytorch.settings.fast_pred_var(state=False),
+                gpytorch.settings.lazily_evaluate_kernels(state=False),
+            ):
+                estimateSingle2DWithWindow(
+                    signal_name,
+                    signal_hist,
+                    bkg_hist,
+                    window,
+                    Path(base_dir) / f"inject_{name}",
+                    rebin_signal=rebin_signal,
+                    **kwargs,
+                )
+
+
+def argEq(x):
+    left, right = x.split("=")
+    return {left: right}
 
 
 def main(args):
+    import gpytorch
+
+    # gpytorch.settings.lazily_evaluate_kernels(state=False)
+    # gpytorch.settings.cholesky_max_tries(1000000)
+    # gpytorch.settings.lazily_evaluate_kernels(state=True)
+    torch.set_default_dtype(torch.float64)
+
     mpl.use("Agg")
     mplhep.style.use("CMS")
     if not args.blind_signal:
@@ -292,6 +334,11 @@ def main(args):
         other_model_data = torch.load(args.use_other_model, weights_only=False)
         other_model = regression.loadModel(other_model_data)
 
+    extra_metadata = {
+        k: v for d in map(argEq, args.metadata or []) for k, v in d.items()
+    }
+
+    logger.info(extra_metadata)
     estimateSingle2D(
         background_path=args.background,
         signal_path=args.signal,
@@ -311,6 +358,7 @@ def main(args):
         min_base_variance=5,
         use_other_model=other_model,
         inject_other_signals=args.inject_other_signals,
+        extra_metadata=extra_metadata,
     )
 
 
@@ -339,8 +387,9 @@ def addToParser(parser):
     parser.add_argument("--injected", type=float, nargs="*", default=[0.0])
     parser.add_argument("-i", "--iterations", type=int, default=100)
     parser.add_argument("--cuda", action="store_true", help="Use cuda", default=False)
-    parser.add_argument("--spread", type=float, default=1.0)
+    parser.add_argument("--spread", type=float, default=1.5)
     parser.add_argument("--inject-other-signals", default=None, type=str, nargs="*")
+    parser.add_argument("--metadata", default=None, type=str, nargs="*")
     parser.add_argument("--use-other-model", type=str)
     parser.add_argument(
         "--blind-signal", default=True, action=argparse.BooleanOptionalAction

fitting/models.py
@@ -121,13 +121,13 @@ def wrapNN(cls_name, kernel):
                 if x[1] is not None
             )
             self.feature_extractor = LargeFeatureExtractor(**nnargs)
-        self.scale_to_bounds = gpytorch.utils.grid.ScaleToBounds(-4.0, 4.0)
+        self.scale_to_bounds = gpytorch.utils.grid.ScaleToBounds(-1.0, 1.0)
 
         def init_weights(m):
             if isinstance(m, torch.nn.Linear):
                 torch.nn.init.xavier_uniform_(m.weight)
 
-        # self.apply(init_weights)
+        self.apply(init_weights)
 
     def forward(self, x1, x2, **params):
         # x1_, x2_ = (
@@ -246,7 +246,9 @@ class NonStatParametric2D(gpytorch.models.ExactGP):
 
 
 class MyNNRBFModel2D(gpytorch.models.ExactGP):
-    def __init__(self, train_x, train_y, likelihood, inducing_ratio=2, num_inducing=None):
+    def __init__(
+        self, train_x, train_y, likelihood, inducing_ratio=1, num_inducing=None
+    ):
 
         if num_inducing and inducing_ratio:
             raise RuntimeError("Cannot have both num inducing and inducing ratio")
@@ -262,49 +264,24 @@ class MyNNRBFModel2D(gpytorch.models.ExactGP):
             self.num_inducing = num_inducing
             ind = train_x[:num_inducing].clone()
 
-        self.mean_module = gpytorch.means.ConstantMean()
-
-        # self.base_covar_module = gpytorch.kernels.ScaleKernel(
-        #     NNMaternKernel(idim=2, odim=2, layer_sizes=(16, 8))
-        # )
-        # base_covar_module = SK(NNMaternKernel(idim=2, odim=2, layer_sizes=(8, 4))) + SK(
-        #     gpytorch.kernels.RBFKernel(ard_num_dims=2)
-        # )
-
-        base_covar_module = SK(gpytorch.kernels.RBFKernel(ard_num_dims=2)) + SK(
-            NNRBFKernel(idim=2, odim=2, layer_sizes=(32, 16, 8))
-        )
-        base_covar_module = SK(NNRBFKernel(idim=2, odim=2, layer_sizes=(40, 20)))
-
-        # base_covar_module = SK(gpytorch.kernels.RBFKernel(ard_num_dims=2)) + SK(
-        #     gpytorch.kernels.MaternKernel(ard_num_dims=2, mu=2.5)
-        # )
+        self.mean_module = gpytorch.means.ZeroMean()
 
         # base_covar_module = SK(gpytorch.kernels.RBFKernel(ard_num_dims=2))
+        # base_covar_module = SK(gpytorch.kernels.MaternKernel(ard_num_dims=2))
+        base_covar_module = SK(gpytorch.kernels.RBFKernel(ard_num_dims=2)) + SK(NNRBFKernel(idim=2, odim=2, layer_sizes=(8, 4)))
 
-        # base_covar_module = SK(NNRBFKernel(idim=2, odim=2, layer_sizes=(8, 4)))
+        # base_covar_module = SK(NNRBFKernel(idim=2, odim=2, layer_sizes=(32, 16)))
+        base_covar_module = SK(NNRBFKernel(idim=2, odim=2, layer_sizes=(8,4))) + SK(NNRBFKernel(idim=2, odim=2, layer_sizes=(8,4)))
 
-        # base_covar_module = NNSMKernel(num_mixtures=4).cuda(e
-        # base_covar_module.initialize_from_data(train_x, train_y)
+        # base_covar_module = SK(gpytorch.kernels.RBFKernel(ard_num_dims=2)) + SK(NNMaternKernel(idim=2, odim=2, layer_sizes=(8, 4)))
 
-        # base_covar_module = gpytorch.kernels.ScaleKernel(
-        #     gpytorch.kernels.RBFKernel(ard_num_dims=2)
+        # self.covar_module = gpytorch.kernels.InducingPointKernel(
+        #     base_covar_module,
+        #     likelihood=likelihood,
+        #     inducing_points=ind,
         # )
 
-        # self.feature_extractor = LargeFeatureExtractor(
-        #     odim=2, idim=2, layer_sizes=(8, 4)
-        # )
-        # self.scale_to_bounds = gpytorch.utils.grid.ScaleToBounds(-1.0, 1.0)
-
-        print("HERE")
-
-        self.covar_module = gpytorch.kernels.InducingPointKernel(
-            base_covar_module,
-            likelihood=likelihood,
-            inducing_points=ind,
-        )
-
-        # self.covar_module = base_covar_module
+        self.covar_module = base_covar_module
 
         # self.covar_module.inducing_points.requires_grad = False
         # gs = gpytorch.utils.grid.choose_grid_size(train_x, 0.25)
@@ -314,6 +291,9 @@ class MyNNRBFModel2D(gpytorch.models.ExactGP):
         #     num_dims=2,
         #     grid_size=gs,
         # )
+
+        # self.feature_extractor = LargeFeatureExtractor(layer_sizes=(16,8))
+        # self.scale_to_bounds = gpytorch.utils.grid.ScaleToBounds(-1.0,1.0)
 
     def forward(self, x):
         # x = self.feature_extractor(x)

fitting/regression.py
@@ -23,6 +23,9 @@ from .utils import dataToHist, computePosterior, chi2Bins
 
 logger = logging.getLogger(__name__)
 
+min_noise = 1e-7
+max_noise=  1e-4
+
 
 @dataclass
 class TrainedModel:
@@ -55,7 +58,6 @@ def getModelingData(trained_model, other_data=None):
         bm = trained_model.blind_mask
     else:
         bm = torch.zeros_like(all_data.Y, dtype=bool)
-
     return all_data, bm
 
 
@@ -70,10 +72,11 @@ def loadModel(trained_model, other_data=None):
     normalized_blinded_data = transform.transform(blinded_data)
     normalized_all_data = transform.transform(all_data)
 
+    logger.info(f"Loading model, learned noise is {trained_model.learned_noise}")
     likelihood = gpytorch.likelihoods.FixedNoiseGaussianLikelihood(
         noise=normalized_blinded_data.V,
         learn_additional_noise=trained_model.learned_noise,
-        noise_constraint=gpytorch.constraints.GreaterThan(1e-10),
+        noise_constraint=gpytorch.constraints.Interval(min_noise,max_noise)
     )
 
     inducing = model_state.get("covar_module.inducing_points")
@@ -106,17 +109,25 @@ def getPosteriorProcess(model, data, transform, extra_noise=None):
         normalized_data,
         slope=transform.transform_y.slope,
         intercept=transform.transform_y.intercept,
+        extra_noise=extra_noise,
     )
-    logger.info(pred_dist.variance)
-    logger.info(f"Extra noise is {extra_noise}")
-    if extra_noise is not None:
-        en = transform.transform_y.iTransformVariances(extra_noise)
-        new_cov = torch.diag(torch.ones(pred_dist.variance.size(0)) * en).detach()
-        to_add = gpytorch.distributions.MultivariateNormal(
-            torch.zeros_like(pred_dist.mean).detach(), new_cov
-        )
-        pred_dist += to_add
-    logger.info(pred_dist.variance)
+    # logger.info(pred_dist.variance)
+    # if extra_noise is not None:
+    #     en = transform.transform_y.iTransformVariances(extra_noise)
+    #     # en = extra_noise
+    #     # logger.info(
+    #     #     f"Extra noise is {transform.transform_y.iTransformVariances(extra_noise)}"
+    #     # )
+    #     logger.info(f"Extra noise is {en}")
+    #     new_cov = torch.diag(torch.ones(pred_dist.variance.size(0)) * en).detach()
+    #     to_add = gpytorch.distributions.MultivariateNormal(
+    #         torch.zeros_like(pred_dist.mean).detach(), torch.sqrt(new_cov)
+    #     )
+    #     # pred_dist = gpytorch.distributions.MultivariateNormal(
+    #     #     pred_dist.mean, pred_dist.covariance_matrix + new_cov
+    #     # )
+    #     # pred_dist += to_add
+    # logger.info(pred_dist.variance)
     return pred_dist
 
 
@@ -170,6 +181,7 @@ class DataValues:
     def fromHistogram(histogram):
         edges = tuple(torch.from_numpy(a.edges) for a in histogram.axes)
         centers = tuple(torch.diff(e) / 2 + e[:-1] for e in edges)
+        print(centers[0].dtype)
         bin_values = torch.from_numpy(histogram.values())
         bin_vars = torch.from_numpy(histogram.variances())
         bin_values = bin_values.T
@@ -223,29 +235,29 @@ def optimizeHyperparams(
         "parameters": [],
     }
 
-    with linear_operator.settings.max_cg_iterations(10000):
-        for i in range(iterations):
-            optimizer.zero_grad()
-            output = model(train_data.X)
-            loss = -mll(output, train_data.Y)
-            loss.backward()
-            optimizer.step()
-            # scheduler.step()
-            # slr = scheduler.get_last_lr()[0]
-            if validate_function is not None and False:
-                c2u, c2b = validate_function(model)
-                training_progress["chi2_unblind"].append(c2u.detach())
-                training_progress["chi2_blind"].append(c2b.detach())
-            if (i % (iterations // 20) == 0) or i == iterations - 1:
-                logger.info(f"Iter {i} (lr={lr:0.4f}): Loss={round(loss.item(),4)}")
-                # for n, k in model.named_parameters():
-                #     logger.info(f"{n}: {k}")
-                c2u, c2b = validate_function(model)
-            torch.cuda.empty_cache()
-            training_progress["loss"].append(loss.detach())
-            training_progress["parameters"].append(
-                dict((x, y.detach()) for x, y in model.named_parameters())
-            )
+    # with linear_operator.settings.max_cg_iterations(10):
+    for i in range(iterations):
+        optimizer.zero_grad()
+        output = model(train_data.X)
+        loss = -mll(output, train_data.Y)
+        loss.backward()
+        optimizer.step()
+        # scheduler.step()
+        # slr = scheduler.get_last_lr()[0]
+        if validate_function is not None and False:
+            c2u, c2b = validate_function(model)
+            training_progress["chi2_unblind"].append(c2u.detach())
+            training_progress["chi2_blind"].append(c2b.detach())
+        if (i % (iterations // 20) == 0) or i == iterations - 1:
+            logger.info(f"Iter {i} (lr={lr:0.4f}): Loss={round(loss.item(),4)}")
+            # for n, k in model.named_parameters():
+            #     logger.info(f"{n}: {k}")
+            c2u, c2b = validate_function(model)
+        torch.cuda.empty_cache()
+        training_progress["loss"].append(loss.detach())
+        training_progress["parameters"].append(
+            dict((x, y.detach()) for x, y in model.named_parameters())
+        )
 
     # for n, k in model.named_parameters():
     #     print(f"{n}: {k}")
@@ -312,7 +324,7 @@ def updateModelNewData(
     likelihood = gpytorch.likelihoods.FixedNoiseGaussianLikelihood(
         noise=train.V,
         learn_additional_noise=learn_noise,
-        noise_constraint=gpytorch.constraints.GreaterThan(1e-20),
+        noise_constraint=gpytorch.constraints.Interval(min_noise,max_noise),
     )
     model.set_train_data(train.X, train.Y, strict=False)
     model.likelhood = likelihood
@@ -358,7 +370,7 @@ def doCompleteRegression(
     else:
         window_mask = ~torch.ones_like(test_data.Y, dtype=bool)
 
-    train_data = test_data[~window_mask][::1]
+    train_data = test_data[~window_mask]
     # print(torch.count_nonzero(domain_mask))
     # print(torch.count_nonzero(window_mask))
     # print(test_data.X.shape)
@@ -382,7 +394,7 @@ def doCompleteRegression(
     likelihood = gpytorch.likelihoods.FixedNoiseGaussianLikelihood(
         noise=train.V,
         learn_additional_noise=learn_noise,
-        noise_constraint=gpytorch.constraints.GreaterThan(1e-4),
+        noise_constraint=gpytorch.constraints.Interval(min_noise,max_noise),
     )
 
     # likelihood = gpytorch.likelihoods.GaussianLikelihood()

fitting/utils.py
@@ -1,8 +1,22 @@
 import linear_operator
 import torch
 import gpytorch
+from rich import print
 from uhi.numpy_plottable import NumPyPlottableHistogram
 import numpy as np
+import logging
+import warnings
+import torch
+
+from linear_operator import settings
+from linear_operator.utils.errors import NanError, NotPSDError
+from linear_operator.utils.warnings import NumericalWarning
+import code
+import readline
+import rlcompleter
+
+
+logger = logging.getLogger(__name__)
 
 
 def pointsToGrid(points_x, points_y, edges, set_unfilled=None):
@@ -40,12 +54,19 @@ def chi2Bins(obs, exp, var, mask=None, min_var=0, power=2):
 
 
 def fixMVN(mvn):
-    X = linear_operator.utils.cholesky.psd_safe_cholesky(mvn.covariance_matrix)
+    with gpytorch.settings.cholesky_max_tries(10000):
+        X = linear_operator.utils.cholesky.psd_safe_cholesky(mvn.covariance_matrix)
     fixed_cv = X @ X.T
     mu = mvn.mean
     fixed = type(mvn)(mu, fixed_cv)
     # assert(torch.allclose(fixed_cv, mvn.covariance_matrix))
     return fixed
+
+
+def fixCov(mat):
+    X = linear_operator.utils.cholesky.psd_safe_cholesky(mat)
+    fixed_cv = X @ X.T
+    return fixed_cv
 
 
 def affineTransformMVN(mvn, slope, intercept):
@@ -54,17 +75,19 @@ def affineTransformMVN(mvn, slope, intercept):
     new_mu = slope * mu + intercept
     d = slope * torch.eye(cm.shape[0], device=slope.device)
     new_cm = d @ cm @ d.T
+    # new_cm = fixCov(new_cm)
     ret = type(mvn)(new_mu, new_cm)
     return ret
 
 
 def getScaledEigenvecs(cov_mat, top=None):
-    linear_operator.utils.cholesky.psd_safe_cholesky(cov_mat)
+    # linear_operator.utils.cholesky.psd_safe_cholesky(cov_mat)
     vals, vecs = torch.linalg.eigh(cov_mat)
     vals = vals.real
     vecs = vecs.real
 
     X = vecs @ torch.diag(torch.sqrt(vals))
+    logger.info(vals)
     assert torch.allclose(X @ X.T, cov_mat)
 
     vals = torch.flip(vals, (0,))
@@ -80,17 +103,52 @@ def getScaledEigenvecs(cov_mat, top=None):
     return eva, eve
 
 
-def computePosterior(model, likelihood, data, slope=None, intercept=None):
-    with torch.no_grad():
+def fixCovar(mat):
+    vals, vecs = torch.linalg.eigh(mat)
+    vals = vals.clamp_min(0.0)
+    covar = vecs @ torch.diag(vals) @ vecs.T
+    psd_covar_C = linear_operator.utils.cholesky.psd_safe_cholesky(covar)
+    covar = psd_covar_C @ psd_covar_C.T
+    return covar
+
+
+def computePosterior(
+    model, likelihood, data, slope=None, intercept=None, extra_noise=None
+):
+    # model.eval()
+    with (
+        torch.no_grad(),
+        gpytorch.settings.fast_computations(
+            covar_root_decomposition=False, log_prob=False, solves=False
+        ),
+        gpytorch.settings.fast_pred_samples(state=False),
+        gpytorch.settings.fast_pred_var(state=False),
+        gpytorch.settings.lazily_evaluate_kernels(state=False),
+    ):
         pred_dist = model(data.X)
-    with gpytorch.settings.cholesky_max_tries(30):
-        psd_pred_dist = fixMVN(pred_dist)
-    if slope is not None and intercept is not None:
-        pred_dist = affineTransformMVN(psd_pred_dist, slope, intercept)
-    else:
-        pred_dist = type(pred_dist)(
-            psd_pred_dist.mean,
-            psd_pred_dist.covariance_matrix.to_dense(),
+
+    pred_dist = fixMVN(pred_dist)
+    # logger.info(f"Initial Transfomed variances are {pred_dist.variance * slope**2}")
+
+    # vars = globals()
+    # vars.update(locals())
+    # vars = {"histogram": h}
+    # readline.set_completer(rlcompleter.Completer(vars).complete)
+    # readline.parse_and_bind("tab: complete")
+    # code.InteractiveConsole(vars).interact()
+    # logger.info(pred_dist.covariance_matrix)
+    # logger.info(pred_dist.covariance_matrix.dtype)
+
+    # logger.info(f"Initial mean is {pred_dist.mean * slope}")
+    if extra_noise is not None:
+        en = extra_noise 
+        new_cov = torch.diag(torch.ones(pred_dist.variance.size(0)) * en).detach()
+        logger.info(f"Adding extra noise to covariance {torch.sqrt(en*slope**2)}")
+        pred_dist = gpytorch.distributions.MultivariateNormal(
+            pred_dist.mean, pred_dist.covariance_matrix + new_cov
         )
+
+    if slope is not None and intercept is not None:
+        pred_dist = affineTransformMVN(pred_dist, slope, intercept)
 
     return pred_dist