a20053caed46e2a558bad8a59f559a744e8f4a8a
kapsiak <kapsi002@umn.edu>
2025-06-13 19:27:30 +0200

    Simple script for making combine condor inputs

---
+++ scripts/mkcombinelist.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+import itertools as it
+import argparse
+from collections import namedtuple
+import re
+
+SignalParts = namedtuple("SignalParts", "coupling stop chargino")
+
+
+def parseArgs():
+    parser = argparse.ArgumentParser(description="Make the list")
+    parser.add_argument("inputs", nargs="+")
+    parser.add_argument("--base")
+    return parser.parse_args()
+
+
+def main():
+    args = parseArgs()
+    inputs = [Path(x) for x in args.inputs]
+    for path in inputs:
+        print(path.relative_to(args.base))
+        
+
+
+if __name__ == "__main__":
+    main()
