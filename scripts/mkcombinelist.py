d974e62a736b53fb6530dc51b932c5b99cdf3573
Charlie Kapsiak <kapsi002@umn.edu>
2025-07-03 18:43:15 -0500

    Use python based glob

--- scripts/mkcombinelist.py
+++ scripts/mkcombinelist.py
@@ -9,15 +9,15 @@ SignalParts = namedtuple("SignalParts", "coupling stop chargino")
 
 def parseArgs():
     parser = argparse.ArgumentParser(description="Make the list")
-    parser.add_argument("inputs", nargs="+")
+    parser.add_argument("inputs")
     parser.add_argument("--base")
     return parser.parse_args()
 
 
 def main():
     args = parseArgs()
-    inputs = [Path(x) for x in args.inputs]
-    for path in inputs:
+    # inputs = [Path(x) for x in args.inputs]
+    for path in Path(".").glob(args.inputs):
         print(path.relative_to(args.base))
         
 
