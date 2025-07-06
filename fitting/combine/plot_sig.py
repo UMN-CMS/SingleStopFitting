a13768767a0a5c97149c2273ff80c2d3a05048b4
kapsiak <kapsi002@umn.edu>
2025-07-05 22:50:34 -0500

    Improve significance plotting

--- fitting/combine/plot_sig.py
+++ fitting/combine/plot_sig.py
@@ -1,4 +1,5 @@
 import json
+import itertools as it
 
 import matplotlib.patches as patches
 import matplotlib.pyplot as plt
@@ -55,21 +56,7 @@ end_yaxis = 2000
 #     d = griddata(vals[:, :2], vals[:, 2], (grid_x, grid_y), method="nearest")
 
 
-def plotSig(data, output_path, coupling="312"):
-    data = np.array(
-        [
-            [x.signal_point.mt, x.signal_point.mx, x.inference_data["significance"]]
-            for x in data
-        ]
-    )
-    fig, ax = plt.subplots()
-    c = ax.scatter(data[:, 0], data[:, 1], c=data[:, 2], s=400)
-    cb = fig.colorbar(c, ax=ax)
-    cb.set_label(r"Significance")
-    ax.set_xlabel(r"$m_{\tilde{t}}$")
-    ax.set_ylabel(r"$m_{\tilde{\chi}}$")
-    addCMS(ax, loc=1)
-
+def commonElements(ax):
     ax.set_xlim(start_xaxis, end_xaxis)
     ax.set_ylim(start_yaxis, end_yaxis)
 
@@ -147,27 +134,41 @@ def plotSig(data, output_path, coupling="312"):
         ),
         arrowprops=dict(arrowstyle="->", lw=2),
     )
-    # ax.annotate(
-    #     "",
-    #     (
-    #         start_points_x + 0.2 * (end_xaxis - start_points_x),
-    #         start_yaxis + 0.15 * (start_points_y - start_yaxis),
-    #     ),
-    #     (
-    #         start_points_x + 0.2 * (end_xaxis - start_points_x),
-    #         start_yaxis + 0.85 * (start_points_y - start_yaxis),
-    #     ),
-    #     arrowprops=dict(arrowstyle="->", lw=2),
-    # )
-
-    fig.tight_layout()
-    fig.savefig(output_path)
 
 
-def plotLim(data, output_path, coupling="312"):
+def plotSig(data, output_path, coupling="312", year="2018"):
     data = np.array(
         [
-            [x.signal_point.mt, x.signal_point.mx, x.inference_data["limit"]]
+            [
+                x.signal_point.mt,
+                x.signal_point.mx,
+                x.inference_data.get("significance", np.nan),
+            ]
+            for x in data
+        ]
+    )
+    print(data)
+    fig, ax = plt.subplots()
+    c = ax.scatter(data[:, 0], data[:, 1], c=data[:, 2], s=400)
+    cb = fig.colorbar(c, ax=ax)
+    cb.set_label(r"Significance")
+    ax.set_xlabel(r"$m_{\tilde{t}}$")
+    ax.set_ylabel(r"$m_{\tilde{\chi}}$")
+    addCMS(ax, year=year, loc=1)
+    commonElements(ax)
+    fig.tight_layout()
+    fig.savefig(output_path)
+    plt.close(fig)
+
+
+def plotLim(data, output_path, coupling="312", year="2018"):
+    data = np.array(
+        [
+            [
+                x.signal_point.mt,
+                x.signal_point.mx,
+                x.inference_data.get("limit", {"50": np.nan})["50"],
+            ]
             for x in data
         ]
     )
@@ -177,26 +178,12 @@ def plotLim(data, output_path, coupling="312"):
     cb.set_label(r"Expected Limit")
     ax.set_xlabel(r"$m_{\tilde{t}}$")
     ax.set_ylabel(r"$m_{\tilde{\chi}}$")
-    addCMS(ax, loc=1)
+    addCMS(ax, loc=1, year=year)
 
+    commonElements(ax)
+    fig.tight_layout()
     fig.savefig(output_path)
-
-
-def plotSigRatio(data, output_path, coupling="312"):
-    data = np.array([[*x, y] for x, y in data.items()])
-    fig, ax = plt.subplots()
-    c = ax.scatter(data[:, 0], data[:, 1], c=data[:, 2], s=800, vmin=0, vmax=2)
-    cb = fig.colorbar(c, ax=ax)
-    cb.set_label(r"Significance Ratio")
-    ax.set_xlabel(r"$m_{\tilde{t}}$")
-    ax.set_ylabel(r"$m_{\tilde{\chi}}$")
-    addCMS(ax, loc=1)
-
-    for x, y, z in data:
-        if z is not None:
-            ax.text(x, y, f"{z:0.2f}", ha="center", size=8)
-
-    fig.savefig(output_path)
+    plt.close(fig)
 
 
 def parseAguments():
@@ -214,35 +201,56 @@ def main():
     # with open(args.input) as f:
     #     data = json.load(f)
     # plotRate(data, args.output, coupling=args.coupling)
-    with open("gathered/2025_06_19_srmc_small_nn.json", "r") as f:
-        data = SignalRunCollection.model_validate_json(f.read())
+    with open("gathered/2025_07_05_small_nn_official.json", "r") as f:
+        base_data = SignalRunCollection.model_validate_json(f.read())
+    dropped_points = [(1000, 800), (1200, 800)]
+    years = [
+        # "2016_preVFP",
+        "2016_postVFP",
+        "2017",
+        "2018",
+        "2022_preEE",
+        "2022_postEE",
+        "2023_preBPix",
+        "2023_postBPix",
+    ]
 
-    dropped_points = [(1000, 800)]
-    year = "2018"
+    for coupling, year in it.product(["312", "313"], years):
 
-    def f(item):
-        return (
-            item.signal_injected == 1
-            and item.signal_point.coupling == "312"
-            and item.signal_point.mx > 300
-            and (item.signal_point.mt, item.signal_point.mx) not in dropped_points
+        def f(item):
+            return (
+                item.signal_injected == 1
+                and item.signal_point.coupling == coupling
+                and item.signal_point.mx > 300
+                and item.signal_point.mt > 900
+                and (item.signal_point.mt, item.signal_point.mx) not in dropped_points
+            )
+
+        data = base_data.filter(year=year, other_filter=f)
+        Path(f"deletemelater/{year}/").mkdir(exist_ok=True, parents=True)
+        plotSig(
+            data,
+            f"deletemelater/{year}/srmc_sig_{coupling}_plot.pdf",
+            coupling=coupling,
+            year=year,
         )
 
-    data = data.filter(year=year, other_filter=f)
+        def f(item):
+            return (
+                item.signal_injected == 0
+                and item.signal_point.coupling == coupling
+                and item.signal_point.mx > 300
+                and item.signal_point.mt > 900
+                and (item.signal_point.mt, item.signal_point.mx) not in dropped_points
+            )
 
-    Path(f"deletemelater/{year}/").mkdir(exist_ok=True, parents=True)
-    plotSig(data, f"deletemelater/{year}/srmc_312_sig_plot.pdf")
-
-    # def f(item):
-    #     return (
-    #         item.signal_injected == 0
-    #         and item.signal_point.coupling == "312"
-    #         and item.signal_point.mx > 300
-    #         and (item.signal_point.mt, item.signal_point.mx) not in dropped_points
-    #     )
-    #
-    # data_0 = data.filter(year=year, other_filter=f)
-    # plotLim(data_0, f"deletemelater/{year}/srmc_312_lim_plot.png")
+        data_0 = base_data.filter(year=year, other_filter=f)
+        plotLim(
+            data_0,
+            f"deletemelater/{year}/srmc_lim_{coupling}_plot.pdf",
+            coupling=coupling,
+            year=year,
+        )
 
 
 if __name__ == "__main__":
--- fitting/plotting/annots.py
+++ fitting/plotting/annots.py
@@ -1,14 +1,14 @@
 import mplhep
 
 
-def addCMS(ax, text="", loc=0):
+def addCMS(ax, text="", loc=0, year="2018"):
     if text is not None:
         text = "\n" + text
     mplhep.cms.label(
         data=True,
         label="Preliminary" + text,
-        year=2018,
-        lumi=59.8,
+        year=year,
+        # lumi=59.8,
         # energy=13,
         ax=ax,
         loc=loc,
